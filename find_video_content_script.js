eaf0fe5fad15bf8e2b05f150c8a8484fa488c104
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-06-22 00:18:44 +0200

    Add YouTube specific toggle button positioned in player's toolbar

--- find_video_content_script.js
+++ find_video_content_script.js
@@ -9,63 +9,154 @@ const findVideoNodes = () => {
         return;
     }
 
+    addStylesheet();                // Stylesheet is only added once even if called multiple times
     videoNodes.forEach((node) => {
         attach(node);
     });
 };
 
-const addStylesheet = () => {
-    console.log('Applying stylesheet');
-    const style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.type = 'text/css';
-    style.href = chrome.extension.getURL('overlay_video.css');
-    (document.head||document.documentElement).appendChild(style);
+// TODO: Maybe pick attacher based on the player's ids if there are any?
+// Like the YouTube player and JW player etc
+const getAttacher = () => {
+    // The regex below is simple but does what it needs to
+    // It utilizes a lookahead, (?=.com), to match everything up until that point
+    // It utilizes a lookbehind, (?<=\/\/www\.), to match everything after //www.
+    // By matching after //www. you get the domain name which is crucial when picking attacher
+    /* const domains = window.location.href.match(new RegExp('(?<=\/\/www\.).*(?=.com)'));
+
+    if (domains.length <= 0) {
+        console.log('Could not decipher domain name from URL');
+        return new Attacher();
+    }
+
+    switch (domains[0]) {
+        case 'youtube':
+            console.log('Picking YouTube attacher');
+            return new YouTubeAttacher();
+        default:
+            return new Attacher();
+    } */
 };
+
+// Function can only be executed once. To prevent multiple repetitive stylesheets
+// from being appended
+const addStylesheet = (function() {
+    let executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+
+            console.log('Applying stylesheet');
+            const style = document.createElement('link');
+            style.rel = 'stylesheet';
+            style.type = 'text/css';
+            style.href = chrome.extension.getURL('overlay_video.css');
+            (document.head||document.documentElement).appendChild(style);
+        }
+    };
+})();
 
 const attach = (video) => {
     console.log('Attaching to', video);
-    attachToggleButton(video);
+    toggleAttacher.attach(video);
+    videoAttacher.attach(video);
 };
-
-const tail = ([x, ...xs]) => xs;
-
-const isEmptyObject = (obj) => (Object.keys(obj).length === 0 && obj.constructor === Object);
 
 // Attach overlay toggle depending on the player
 // For certain reoccuring players you attach the toggle button
 // in the player's toolbar
 // For unrecognized players; apply default button placement
-const attachToggleButton = (video) => {
-    console.log('Attaching toggle button', video);
-    // Create button container
-    const container = document.createElement('div');
-    container.id = 'ss-container';
-    // Create inner container(to enable CSS hack for a perfect square shape)
-    const innerContainer = document.createElement('div');
-    const toggle = document.createElement('img');
-    toggle.src = chrome.runtime.getURL('images/icon.png');
-    innerContainer.appendChild(toggle);
-    container.appendChild(innerContainer);
+// YouTube: #ytd-player -> .ytp-right-controls
+// In order to put the toggle button within .ytp-right-controls
+// you need to verify that the video resides within #ytd-player
+const toggleAttacher = {
+    attach: (video) => {
+        // Is the video node a YouTube player?
+        const youtubePlayer = document.querySelector('#ytd-player');
 
-    // Ensure that a container has not yet already been injected
-    if (video.nextSibling && video.nextSibling.id === 'ss-container') {
-        console.log('Toggle container already in DOM');
-        return;
-    }
-
-    // TODO: Place somewhere different, has nothing to do with attaching toggle
-    addStylesheet();
-    // Event listener should be on the element in order to be able to stop the propagation
-    /* document.addEventListener('click', (e) => {
-        if (e.target && e.target.id== 'ss-container') {
-              toggleOnClick(e);
+        if (youtubePlayer !== null) {
+            // Is the video node in question a YouTube player video?
+            // I.e a descendant of the YouTube player node
+            if (youtubePlayer.contains(video)) {
+                const controlContainer = youtubePlayer.querySelector('.ytp-right-controls');
+                return toggleAttacher.attachYouTube(controlContainer);
+            }
         }
-    }); */
-    container.addEventListener('click', toggleOnClick, false);
-    // Video nodes can't contain elements, place it adjacent to the video
-    // video.appendChild(container);
-    video.parentNode.insertBefore(container, video.nextSibling);
+
+        // No known player could be identified; apply default toggle
+        return toggleAttacher.attachDefault(video);
+    },
+
+    /**
+     * @argument controlsNode is the container in YouTube's toolbar dedicated to controls
+     */
+    attachYouTube: (controlsNode) => {
+        console.log('Attaching YouTube toggle button', controlsNode);
+
+        // Create button container
+        /* const container = document.createElement('div');
+        container.id = 'ss-youtube-container';
+        // Create inner container(to enable CSS hack for a perfect square shape)
+        const innerContainer = document.createElement('div');
+        const toggle = document.createElement('img');
+        toggle.src = chrome.runtime.getURL('images/icon.png');
+        // Construct elements
+        innerContainer.appendChild(toggle);
+        container.appendChild(innerContainer); */
+
+        const button = document.createElement('button');
+        button.id = 'ss-youtube-container';
+        // YouTube specific classes
+        button.classList.add('playerButton', 'ytp-button');
+        const buttonImage = document.createElement('img');
+        buttonImage.src = chrome.runtime.getURL('images/IconTransparent.png');
+        buttonImage.classList.add('playerButtonImage');
+        button.appendChild(buttonImage);
+
+        // Ensure that a container has not yet already been injected
+        if (controlsNode.querySelector('#ss-youtube-container') !== null) {
+            console.log('Toggle container already in DOM');
+            return;
+        }
+
+        toggleAttacher.addToggleListener(button);
+        controlsNode.prepend(button);
+    },
+
+    attachDefault: (video) => {
+        console.log('Attaching default toggle button', video);
+        // Create button container
+        const container = document.createElement('div');
+        container.id = 'ss-default-container';
+        // Create inner container(to enable CSS hack for a perfect square shape)
+        const innerContainer = document.createElement('div');
+        const toggle = document.createElement('img');
+        toggle.src = chrome.runtime.getURL('images/Icon.png');
+        // Construct elements
+        innerContainer.appendChild(toggle);
+        container.appendChild(innerContainer);
+
+        // Ensure that a container has not yet already been injected
+        if (video.nextSibling && video.nextSibling.id === 'ss-default-container') {
+            console.log('Toggle container already in DOM');
+            return;
+        }
+
+        // Event listener should be on the element in order to be able to stop the propagation
+        /* document.addEventListener('click', (e) => {
+            if (e.target && e.target.id== 'ss-container') {
+                toggleOnClick(e);
+            }
+        }); */
+        toggleAttacher.addToggleListener(container);
+        // Video nodes can't contain elements, place it adjacent to the video
+        // video.appendChild(container);
+        video.parentNode.insertBefore(container, video.nextSibling);
+    },
+
+    addToggleListener: (toggle) => {
+        toggle.addEventListener('click', toggleOnClick, false);
+    },
 };
 
 const toggleOnClick = (e) => {
@@ -74,8 +165,16 @@ const toggleOnClick = (e) => {
     e.preventDefault();
 };
 
+const videoAttacher = {
+    attach: (video) => {
+        console.log('Attaching video');
+    },
+};
+
+// Start running the script by analysing the current DOM for video nodes
 findVideoNodes();
 
+// Continue monitering the DOM for changes, in particular, additions of video nodes
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         const addedNodes = [ ...mutation.addedNodes ];          // Converted to array for simplicity
